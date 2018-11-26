80abd4db7982a77b69a5878b26559a1a93efd52b
Mark Mansi <markm@cs.wisc.edu>
2018-11-26 10:10:38 -0600

    fix calibrate script

--- src/bin/time_calibrate.rs
+++ src/bin/time_calibrate.rs
@@ -7,9 +7,14 @@ extern crate paperexp;
 
 use paperexp::{rdtsc, vmcall_calibrate, vmcall_nop};
 
+use std::fs::OpenOptions;
+use std::io::Write;
+
 fn main() {
     const ACC: i64 = 100000;
-    const EPSILON: i64 = 100;
+    const EPSILON: i64 = 50;
+
+    let mut devnull = OpenOptions::new().write(true).open("/dev/null").unwrap();
 
     loop {
         let mut sum: i64 = 0;
@@ -17,9 +22,11 @@ fn main() {
             let start = rdtsc() as i64;
             vmcall_nop();
             sum += rdtsc() as i64 - start;
+            writeln!(devnull, "").unwrap();
         }
 
         let avg = sum / ACC;
+        println!("avg {}", avg);
         let too_low = avg > 0;
         if avg.abs() > EPSILON {
             vmcall_calibrate(too_low);

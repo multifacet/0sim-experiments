3a7f31d07773b610abb0a790c8a8f06d07322b5a
Mark Mansi <markm@cs.wisc.edu>
2019-04-30 11:35:50 -0500

    add flag for local and nonlocal accesses

--- src/bin/locality_mem_access.rs
+++ src/bin/locality_mem_access.rs
@@ -5,6 +5,8 @@
 
 use bmk_linux::timing::rdtsc;
 
+use clap::clap_app;
+
 use libc::{
     mmap as libc_mmap, MAP_ANONYMOUS, MAP_FAILED, MAP_POPULATE, MAP_PRIVATE, PROT_READ, PROT_WRITE,
 };
@@ -12,6 +14,20 @@ use libc::{
 use rand::Rng;
 
 fn main() {
+    let matches = clap_app! { locality_mem_access =>
+        (about: "Measures the latency difference in memory access patterns with strong and poor
+                 spatial locality")
+        (@group MODE =>
+            (@attributes +required)
+            (@arg local: -l "Access memory with strong locality")
+            (@arg nonlocal: -n "Access memory with poor locality")
+        )
+
+    }
+    .get_matches();
+
+    let is_local = matches.is_present("local");
+
     // Mmap memory for the experiment
     let mapped = unsafe {
         let addr = libc_mmap(
@@ -38,34 +54,34 @@ fn main() {
     }
 
     // Now touch these a lot and time it
-    for _ in 0..10000 {
-        for i in 0..8 {
+    if is_local {
+        for _ in 0..10000 {
+            for i in 0..8 {
+                let start = rdtsc();
+                unsafe {
+                    *mapped.offset((i << 12) as isize) = 8;
+                }
+                let end = rdtsc();
+
+                println!("{}", end - start);
+            }
+        }
+    } else {
+        let mut rng = rand::thread_rng();
+
+        // Now do something that has terrible performance
+        // - lots of cache and TLB misses
+        // - random behavior to avoid prefetchers
+        for _ in 0..80000 {
+            let i: isize = rng.gen_range(0, (4 << 30) >> 12);
+
             let start = rdtsc();
             unsafe {
-                *mapped.offset((i << 12) as isize) = 8;
+                *mapped.offset(i << 12) = 9;
             }
             let end = rdtsc();
 
             println!("{}", end - start);
         }
-    }
-
-    println!("========");
-
-    let mut rng = rand::thread_rng();
-
-    // Now do something that has terrible performance
-    // - lots of cache and TLB misses
-    // - random behavior to avoid prefetchers
-    for _ in 0..80000 {
-        let i: isize = rng.gen_range(0, (4 << 30) >> 12);
-
-        let start = rdtsc();
-        unsafe {
-            *mapped.offset(i << 12) = 9;
-        }
-        let end = rdtsc();
-
-        println!("{}", end - start);
     }
 }
